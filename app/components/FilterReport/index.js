028741bf6acb0d0e31719b4e4d6e666feb02480e
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-08-06 14:42:33 +0700

    refactoring code.

--- app/components/FilterReport/index.js
+++ app/components/FilterReport/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import DatePickerComponent from "../DateTimePicker/DatePickerComponent";
 import RangePickerComponent from "../DateTimePicker/RangePickerComponent";
 
--- app/containers/App/constants.js
+++
@@ -1,4 +0,0 @@
-export const SET_USER_NAME = 'App/SET_USER_NAME'
-export const SET_CURRENT_USER = 'App/SET_CURRENT_USER'
-export const LOAD_LOCAL_STORAGE = 'App/LOAD_LOCAL_STORAGE'
-export const context = 'global'
