599cbef94b2b04a03a91e1399be016261e7752ae
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-08-08 14:15:27 +0700

    feat: add PermissionRoute for user.

---
+++ app/utils/PermissionRoute.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { selectIsAuthenticated, selectUser } from "../containers/Auth/selectors";
+import NoPermision from "./NoPermission";
+
+const PermissionRoute = ({component: Component, user, role, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        (user.role === role || user.role === role[0] || user.role === role[1]) ? (
+          <Component {...props} />
+        ) : (
+          <NoPermision location={props.location}/>
+        )
+      }
+    />
+  );
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: selectIsAuthenticated(state),
+  user: selectUser(state)
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(PermissionRoute);
